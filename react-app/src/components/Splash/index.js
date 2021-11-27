82d46e4a4d100e0af7e8bcf8f0eafec59eab799d
aryan <aryanattula@gmail.com>
2021-11-26 19:05:25 -0500

    splash text working

--- react-app/src/components/Splash/index.js
+++ react-app/src/components/Splash/index.js
@@ -4,12 +4,13 @@ import { Redirect } from 'react-router-dom';
 import './Splash.css'
 import LoginForm from '../auth/LoginForm' 
 import SignUpForm from '../auth/SignUpForm'  
+import { FiPlay } from "react-icons/fi";
     
 function Splash() {     
-    const [toggleLogin, setToggleLogin ] = useState(true) 
+    const [toggleLogin, setToggleLogin ] = useState(true)   
     const [currentBGImage, setCurrentBGImage] = useState(0);
     const [currentHeader, setCurrentheader] = useState(0); 
-
+    const [formtoggle, setFormToggle] = useState(false)  
     const splashPhotos = [
         'https://ziba-aa.s3.amazonaws.com/splash4.jpg',
         'https://ziba-aa.s3.amazonaws.com/splash5.jpg',
@@ -55,43 +56,61 @@ function Splash() {
 
   
     return (
-        <>
-        <div className='SplashscrollContainer'>   
-            <div className='Splashslider'>
-                <div className='Splashcarousel'>
-                    <div className='Splashinner'>
-                    <span class="change"></span> 
-                    </div> 
+    <>
+        <div className='SplashPageWrapper'>  
+            <div className='footer'>     
+                <div className='FooterContent'>
+                    <div className='FooterText'>Ziba</div>    
                 </div>
-                <div className='Splashtext'>Social Media, Reimagined</div>    
+                <div className='FooterCarText'>
+                    <h1 className='FootPart1Anim'>A moment of inspiration. A moment of inspiration. A moment of inspiration. A moment of inspiration. A moment of inspiration. A moment of inspiration. A moment of inspiration. A moment of inspiration. </h1>
+                    <h1 className='FootPart2Anim'>A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. A chance encounter. </h1>
+                    <h1 className='FootPart1Anim'>A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. A touch of beauty. </h1>
+                    <h1 className='FootPart2Anim'>A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. A fleeting memory. </h1>
+                    <h1 className='FootPart1Anim'>A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality. A dose of reality.  </h1>  
+                </div>
             </div>
-        </div>
-        <div className='imagebackground'> 
-        {splashPhotos.map((image, i) => (
-            <img 
-            id={i}
-            key={i}
-            className={i === currentBGImage ? `image active` : 'image'}
-            src={image}
-            style={{zIndex: `-${i + 1}`}} 
-            alt='splash'
-            />  
-        ))}   
-        <div>
-        </div>
-        <div className='topbox'>  
+            <div className='imagebackground'> 
+            {splashPhotos.map((image, i) => (
+                <img 
+                id={i}
+                key={i}
+                className={i === currentBGImage ? `image active` : 'image'}
+                src={image}
+                style={{zIndex: `-${i + 1}`}} 
+                alt='splash'
+                />  
+            ))}   
             <div>
-            {(toggleLogin === true) ? <LoginForm/>  :  <SignUpForm/> }      
-            </div>     
-            <div className="redirectContainer">    
-                <div className="redirectText">   
-                    {(toggleLogin === true) ? "Don't have an account?" :  "Already Have an Account?" }   
-                    <button onClick={() => setToggleLogin(!toggleLogin)}className='redirectSignUp'>Click Here</button>
-                </div>    
+
+
+
+
+                
+        </div>
+        {(formtoggle == false) ? <div className='SplashTopRight'>
+            <FiPlay onClick={() => setFormToggle(true)}/> 
+            
+        </div> :   
+                    <div className='topbox'>  
+                    <div>
+                    {(toggleLogin === true) ? <LoginForm/>  :  <SignUpForm/> }      
+                    </div>     
+                    <div className="redirectContainer">    
+                        <div className="redirectText">   
+                            {(toggleLogin === true) ? "Don't have an account?" :  "Already Have an Account?" }   
+                            <button onClick={() => setToggleLogin(!toggleLogin)}className='redirectSignUp'>Click Here</button>
+                        </div>    
+                        </div>
+                    </div>  }
                 </div>
-            </div>  
-        </div> 
-        </>
+     
+         
+
+
+
+        </div>
+    </>
     );
 };
 
